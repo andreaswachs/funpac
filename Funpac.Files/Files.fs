1b25914971f0353c2f234d4e94b25bf5e1896ecb
Andreas Wachs <ahja@itu.dk>
2022-04-13 22:29:13 +0200

    Feature/folders lib (#3)
    * Begin implementing
    
    * Add folders library
    
    * Remove default library file
    
    * Install yaml dependency
    
    * Ignore idea dir
    
    * Add default config
    
    * ADd default files

--- Funpac.Files/Files.fs
+++ Funpac.Files/Files.fs
@@ -6,6 +6,8 @@ module Funpac.Files
     open Folders
     open Legivel
 
+    // TODO: If needing to fetch the deafult file, also get the related default files
+
     let templateConfigFileUrl : string =
         "https://raw.githubusercontent.com/andreaswachs/issues-to-md/main/files/1.md"
         
